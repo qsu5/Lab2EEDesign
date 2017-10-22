80edf7be885da3e390c9c425c19919105a827d6a
Qingyang Su <Ken@wireless-nat-inside.uiowa.edu>
2017-10-22 15:18:47 -0500

    commit the sending side

--- lab2.py
+++ lab2.py
@@ -10,12 +10,12 @@ CHANNELS = 1
 RATE = 48000
 INPUT_BLOCK_TIME = 0.02
 INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-UDP_IP = "2620:0:e50:1400:f104:2925:f242:be00"
+UDP_IP = "2620:0:e50:1400:116d:6931:d334:6d9d"
 UDP_PORT = 8000
 
 sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 audio = pyaudio.PyAudio()
-stream = audio.open(format = FORMAT, channels = CHANNELS, rate=RATE, input = True, frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+stream = audio.open(format = FORMAT, channels = CHANNELS, rate=RATE, input = True, frames_per_buffer = 50*INPUT_FRAMES_PER_BLOCK)
 #streamout = audio.open(format = FORMAT, channels = CHANNELS, rate= RATE,output=True, frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
 # enc = Encoder(RATE,CHANNELS,constants.APPLICATION_VOIP)
 # dec = Decoder(RATE,CHANNELS,)
@@ -25,7 +25,7 @@ errorCount = 0
 i = 0
 while(1):
 	try:
-		raw_data = stream.read(1)
+		raw_data = stream.read(INPUT_FRAMES_PER_BLOCK)
 		serial = struct.pack('q',i)
 		sock.sendto(serial+raw_data,(UDP_IP,UDP_PORT))
 		i+=1
