f36067eddfc89c277b697bb4116bfdfb7538c958
Qingyang Su <Ken@wireless-nat-inside.uiowa.edu>
2017-10-24 14:34:23 -0500

    after office hour about codec

--- lab2.py
+++ lab2.py
@@ -10,7 +10,7 @@ CHANNELS = 1
 RATE = 48000
 INPUT_BLOCK_TIME = 0.02
 INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-UDP_IP = "2620:0:e50:1400:116d:6931:d334:6d9d"
+UDP_IP = "2620:0:e50:1400:70cf:621c:8b4c:7bd9"
 UDP_PORT = 8000
 
 sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
@@ -26,14 +26,14 @@ i = 0
 while(1):
 	try:
 		raw_data = stream.read(INPUT_FRAMES_PER_BLOCK)
-		serial = struct.pack('q',i)
+		
 		i+=1
 		print i
 		encdata = ""
 		for x in raw_data:
 			encdata += Encoder.encode(enc,x,INPUT_FRAMES_PER_BLOCK)
-
-
+		serial = struct.pack('q',i)
+		print len(encdata)
 
 		sock.sendto(serial+encdata,(UDP_IP,UDP_PORT))
 
--- lab2Rec.py
+++ lab2Rec.py
@@ -4,6 +4,7 @@ import socket
 from opuslib import Encoder, Decoder
 from opuslib.api import constants
 from opuslib.exceptions import OpusError
+import sys
 
 FORMAT = pyaudio.paInt16 
 CHANNELS = 1
@@ -23,13 +24,19 @@ dec = Decoder(RATE,CHANNELS)
 ###############receiving################
     
 while True:
-	data, addr = sock.recvfrom(INPUT_FRAMES_PER_BLOCK) # buffer size is 1024 bytes
-	print "Raw Data: ",data
-	while len(data) > 0:
-		print "data"
-		decdata = []
-		for x in data:
-			decdata.append(Decoder.decode(dec,x,INPUT_FRAMES_PER_BLOCK))
-		streamout.write(decdata)
-		data = []
+	data = sock.recvfrom(136) # buffer size is 1024 bytes
+	print len(data)
+	try:
+		print "!!"
+		assert len(data) == 136
+		print "??"
+
+	except AssertionError:
+		print 'Didn\'t get full packet!'
+		#sys.exit(1)
+	(serial,)=struct.unpack('q',data[:8])
+	raw_audio = Decoder.decode(dec,data[8:],INPUT_FRAMES_PER_BLOCK)
+	print "Raw Data: ",raw_audio
+	streamout.write(raw_audio)
+
 	
