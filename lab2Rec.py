0721a57f847440d1d4f2f5f5fd9985297cad638e
Qingyang Su <Ken@wireless-nat-inside.uiowa.edu>
2017-10-24 15:21:19 -0500

    receiving side

--- lab2Rec.py
+++ lab2Rec.py
@@ -1,8 +1,7 @@
 import pyaudio
 import struct
 import socket
-from opuslib import Encoder, Decoder
-from opuslib.api import constants
+from opuslib.api import constants,decoder
 from opuslib.exceptions import OpusError
 import sys
 
@@ -19,7 +18,8 @@ sock.bind(("",UDP_PORT))
 audio = pyaudio.PyAudio()
 
 streamout = audio.open(format = FORMAT, channels = CHANNELS, rate= RATE, output=True, frames_per_buffer = INPUT_FRAMES_PER_BLOCK*50)
-dec = Decoder(RATE,CHANNELS)
+
+dec = decoder.create(RATE,CHANNELS)
 
 ###############receiving################
     
@@ -27,15 +27,12 @@ while True:
 	data = sock.recvfrom(136) # buffer size is 1024 bytes
 	print len(data)
 	try:
-		print "!!"
 		assert len(data) == 136
-		print "??"
-
 	except AssertionError:
 		print 'Didn\'t get full packet!'
 		#sys.exit(1)
 	(serial,)=struct.unpack('q',data[:8])
-	raw_audio = Decoder.decode(dec,data[8:],INPUT_FRAMES_PER_BLOCK)
+	raw_audio = decoder.decode(dec,data[8:],INPUT_FRAMES_PER_BLOCK)
 	print "Raw Data: ",raw_audio
 	streamout.write(raw_audio)
 
