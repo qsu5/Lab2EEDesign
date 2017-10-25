081dd119f55b171712a96e14fa0887e4032d8cc9
Qingyang Su <Ken@wireless-nat-inside.uiowa.edu>
2017-10-25 16:20:48 -0500

    add jitter buffer

--- lab2Rec.py
+++ lab2Rec.py
@@ -3,7 +3,51 @@ import struct
 import socket
 from opuslib.api import constants,decoder
 from opuslib.exceptions import OpusError
+import threading
+import Queue
+import heapq
 import sys
+
+###############receiving################
+class recvAndDecode(threading.Thread): 
+	def __init__(self, sock, dec,buf):
+		threading.Thread.__init__(self)
+		self.sock = sock
+		self.dec = dec
+		self.buf = buf
+
+	def run(self):	
+		while True:
+			data = sock.recvfrom(INPUT_FRAMES_PER_BLOCK) # buffer size is 1024 bytes
+			try:
+				assert len(data[0]) == 136
+				(serial,)=struct.unpack('q',data[0][:8])
+				raw_audio = decoder.decode(dec,data[0][8:],len(data[0][8:]),INPUT_FRAMES_PER_BLOCK, False, 1)
+				#try:
+				if len(buf)<5:
+					heapq.heappush(buf,((serial,raw_audio)))
+				print len(buf)
+				#except 
+
+				#print "Raw Data: ",raw_audio
+				
+			except AssertionError:
+				print 'Didn\'t get full packet!'
+				sys.exit(1)
+class playOut(threading.Thread):
+	def __init__(self,streamout,buf):
+		threading.Thread.__init__(self)
+		self.streamout=streamout
+		self.buf = buf
+
+	def run(self):
+		while True:
+			if len(buf)>0:
+				print "try"
+				(serial,raw_audio) = heapq.heappop(buf)
+				print serial
+				streamout.write(raw_audio)
+
 
 FORMAT = pyaudio.paInt16 
 CHANNELS = 1
@@ -16,25 +60,15 @@ UDP_PORT = 8000
 sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 sock.bind(("",UDP_PORT))
 audio = pyaudio.PyAudio()
-
 streamout = audio.open(format = FORMAT, channels = CHANNELS, rate= RATE, output=True, frames_per_buffer = INPUT_FRAMES_PER_BLOCK*50)
-
 dec = decoder.create(RATE,CHANNELS)
 
-###############receiving################
-    
-while True:
-	data = sock.recvfrom(INPUT_FRAMES_PER_BLOCK) # buffer size is 1024 bytes
-	print len(data[0])
-	try:
-		assert len(data[0]) == 136
-		(serial,)=struct.unpack('q',data[0][:8])
-		raw_audio = decoder.decode(dec,data[0][8:],len(data[0][8:]),INPUT_FRAMES_PER_BLOCK, False, 1)
-		#print "Raw Data: ",raw_audio
-		streamout.write(raw_audio)
-	except AssertionError:
-		print 'Didn\'t get full packet!'
-		sys.exit(1)
-	
+buf=[]
+heapq.heapify(buf)
+heapq._heapify_max(buf)
 
+a=recvAndDecode(sock,dec,buf)
+b=playOut(streamout,buf)
+a.start()
+b.start()
 	
